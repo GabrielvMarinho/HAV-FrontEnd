8d93d6fbc0a972ff4a9680886dfdd9cca5345fa5
GABRIEL VOLLES MARINHO <gabriel_v_marinho@estudante.sesisenai.org.br>
2025-02-19 14:01:28 -0300

    centralizado texto

--- elementos_hav/app/components/Title.tsx
+++ elementos_hav/app/components/Title.tsx
@@ -4,14 +4,14 @@ import './components.css';
 export default function Title(props: {size :string, tag :string, text :string}){
     if(props.tag=="h1"){
         return (
-            <div style={{display: "flex", flexDirection: "column", gap:"8px"}}>
+            <div className="title" style={{display: "flex", flexDirection: "column"}}>
                 <h1 className={props.size+"Title"}>{props.text.toUpperCase()}</h1>
                 <div className="titleLine"></div>
             </div>
         );
     }
     return ( 
-        <div style={{display: "flex", flexDirection: "column", gap:"8px"}}>
+        <div className="title" style={{display: "flex", flexDirection: "column"}}>
                 <h2 className={props.size+"Title"}>{props.text.toUpperCase()}</h2>
                 <div className="titleLine"></div>
             </div>
