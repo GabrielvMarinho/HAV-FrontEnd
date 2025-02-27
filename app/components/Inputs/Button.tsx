fb39f9efa6ca43738bbec14832cdc2de9b9bf91c
Nathan João Lima <147559563+nathanjoao@users.noreply.github.com>
2025-02-27 15:19:16 -0300

    eu estava codando na main sem querer, fiz a fita do card do imovel

--- app/components/Inputs/Button.tsx
+++ app/components/Inputs/Button.tsx
@@ -4,11 +4,7 @@ import './css/style.css';
 export default function Button(props: {size: string; text:string; func: () => void}){
     return (
         <div>
-<<<<<<< HEAD
-            <button className={props.size+"Button button darkHover"}>{props.text.toUpperCase()}</button>
-=======
             <button onClick={props.func} className={props.size+"Button button darkHover"}>{props.text.toUpperCase()}</button>
->>>>>>> main
         </div>
     );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -18,65 +18,59 @@ import SearchBarDesktop from "./components/Filters/SearchBar";
 import Modal from "./components/Modal/Modal";
 import SelectBox from "./components/Inputs/SelectBox";
 import Link from "next/link";
-
 import ActionButton from "./components/Inputs/ActionButton";
-
 import Footer from "./components/Footer/Footer";
 import HeaderAdm from "./components/Header/HeaderAdm";
+import TapeCardImovel from "./components/Information/TapeCardImovel";
 
 export default function Home() {
-  const checkSelects = function(){
-    var itens = document.body.querySelectorAll("[datatype='data-select-box-table-list'].selectedBox");
-    
-    console.log(itens)
-  }
-  return (
-    <>
-      <HeaderAdm width={25.9} height={25.4} color=""/>
-      <Link href="/Imoveis">clique aqui</Link>
-      <SelectedStar width={54} height={54} color={"#001111"}/>
-      <NotSelectedStar width={54} height={54} color={"#001111"}/>
-      <DocumentIcon width={211} height={211} color={"#501010"}/>
-      <Title tag="h1" text="titulo"/>
+      const checkSelects = function () {
+            var itens = document.body.querySelectorAll("[datatype='data-select-box-table-list'].selectedBox");
 
-      <Title tag="h2" text="titulo"/>
-      <Button func={checkSelects} size="small" text="adnasidunasd"/>
-      <Button func={checkSelects} size="medium" text="acesse o chat"/>
-      <Button func={checkSelects} size="large" text="acesse o chat"/>
-      <Modal id = "x" trigger={
-              <Button func={checkSelects} size="large" text="abrir modal"/>
-      }></Modal>
+            console.log(itens)
+      }
+      return (
+            <>
+                  <HeaderAdm width={25.9} height={25.4} color="" />
+                  <Link href="/Imoveis">clique aqui</Link>
+                  <SelectedStar width={54} height={54} color={"#001111"} />
+                  <NotSelectedStar width={54} height={54} color={"#001111"} />
+                  <DocumentIcon width={211} height={211} color={"#501010"} />
+                  <Title tag="h1" text="titulo" />
 
-      
-      <TableList size="large" titles={["finalidade", "tipo imóvel",  "email", "proprietário"]} 
-      data={[["COMPRA", "APARTAMENTO", "asdaoid@gmail.com", "Bianca Isabela Vaz"], 
-      ["COMPRA ", "APARTAMENTO", "efvvv@gmail.com", "Bianca Isabela Vaz"]]}/>
-      
-        
-      
-      <Button func={checkSelects} size="large" text="mostrar todas as selectbox da pagina"/>
-
-      <Filter size="medium" 
-      inputs={[<InputText size="medium" text="Nome" placeholder="ex: Bianca" id="nome"/>,
-      <InputText size="medium" text="Email" placeholder="joao@gmail.com" id="email"/>,
-      <InputText size="medium" text="Telefone" placeholder="ex: 672983579" id="telefone"/>,
-      <InputText size="medium" text="CPF" placeholder="ex: 67298357955" id="cpf"/>,
-      <InputDropdown size="medium" text="Status" id="status" 
-      options={[['sssssss', "Indisponível"], ["bia", 'Disponível'], ["bia", 'Alugado'], ["bia", 'Vendido']]}/>,
-      <SlideRange min={50000} max={2000000} step={10000}/>]}/>
-      <SearchBarDesktop placeholder="Busca:"/>   
-      <StarFavorite width={211} height={211} color={"#501010"} selected={true}/>
-      <Dropdown options={[['sssssss', "Mais Próximo"], ["bia", 'Preço Mais Alto']]}/>
-      <InputText id="email" size="small" text="texto" placeholder="hint"/>
-      <InputText id="email" size="medium" text="texto" placeholder="hint"/>
-      <InputText id="email" size="large" text="texto" placeholder="hint"/>
-      <InputText id="email" size="extraLarge" text="texto" placeholder="hint"/>
-      <NavBarAdm/>
-      <InputText size="small" text="CPF" placeholder="hint" id="email"/>
-      <ArrowIcon width={111} height={111} color={"#501010"}/>
-      <ActionButton width={25} height={25} color="" context="admin"/>
-      <Footer width={25} height={25} color=""/>
-      </>
+                  <Title tag="h2" text="titulo" />
+                  <Button func={checkSelects} size="small" text="adnasidunasd" />
+                  <Button func={checkSelects} size="medium" text="acesse o chat" />
+                  <Button func={checkSelects} size="large" text="acesse o chat" />
+                  <Modal id="x" trigger={
+                        <Button func={checkSelects} size="large" text="abrir modal" />
+                  }></Modal>
+                  <TableList size="large" titles={["finalidade", "tipo imóvel", "email", "proprietário"]}
+                        data={[["COMPRA", "APARTAMENTO", "asdaoid@gmail.com", "Bianca Isabela Vaz"],
+                        ["COMPRA ", "APARTAMENTO", "efvvv@gmail.com", "Bianca Isabela Vaz"]]} />
+                  <Button func={checkSelects} size="large" text="mostrar todas as selectbox da pagina" />
+                  <Filter size="medium"
+                        inputs={[<InputText size="medium" text="Nome" placeholder="ex: Bianca" id="nome" />,
+                        <InputText size="medium" text="Email" placeholder="joao@gmail.com" id="email" />,
+                        <InputText size="medium" text="Telefone" placeholder="ex: 672983579" id="telefone" />,
+                        <InputText size="medium" text="CPF" placeholder="ex: 67298357955" id="cpf" />,
+                        <InputDropdown size="medium" text="Status" id="status"
+                              options={[['sssssss', "Indisponível"], ["bia", 'Disponível'], ["bia", 'Alugado'], ["bia", 'Vendido']]} />,
+                        <SlideRange min={50000} max={2000000} step={10000} />]} />
+                  <SearchBarDesktop placeholder="Busca:" />
+                  <StarFavorite width={211} height={211} color={"#501010"} selected={true} />
+                  <Dropdown options={[['sssssss', "Mais Próximo"], ["bia", 'Preço Mais Alto']]} />
+                  <InputText id="email" size="small" text="texto" placeholder="hint" />
+                  <InputText id="email" size="medium" text="texto" placeholder="hint" />
+                  <InputText id="email" size="large" text="texto" placeholder="hint" />
+                  <InputText id="email" size="extraLarge" text="texto" placeholder="hint" />
+                  <NavBarAdm />
+                  <InputText size="small" text="CPF" placeholder="hint" id="email" />
+                  <ArrowIcon width={111} height={111} color={"#501010"} />
+                  <ActionButton width={25} height={25} color="" context="admin" />
+                  <TapeCardImovel/>
+                  <Footer width={25} height={25} color="" />
+            </>
       );
 
 }
