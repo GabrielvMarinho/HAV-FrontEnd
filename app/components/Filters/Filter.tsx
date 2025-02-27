516f6370fea2396203aef3d6ef86324518e3d05d
Nathan João Lima <147559563+nathanjoao@users.noreply.github.com>
2025-02-27 19:18:57 -0300

    Adicionando os botões Comprar/Alugar ao filtro

--- app/components/Filters/Filter.tsx
+++ app/components/Filters/Filter.tsx
@@ -2,11 +2,13 @@ import { ReactNode } from "react";
 import SearchIcon from "../IconsTSX/SearchIcon";
 import "../../variables.css"
 import './css/style.css';
-
+import ButtonComprarAlugar from "../Inputs/ButtonComprarAlugar";
 
 export default function Filter(props: {size :string, inputs :ReactNode[]}){
     return(
         <div className="filterSide">
+            <p style={{fontSize: "var(--text-s)", color: "var(--text-white)", opacity: "0.6"}}>Finalidade</p>
+            <ButtonComprarAlugar/>
             {props.inputs.map( input =>
             <>{input}</>
           )
