6e5246b3ab8de26ebc5d16df3bdcfb61a6b4c12e
Nathan João Lima <147559563+nathanjoao@users.noreply.github.com>
2025-03-04 14:28:04 -0300

    iniciando grafico

--- app/RelatorioEAnalise/page.tsx
+++ app/RelatorioEAnalise/page.tsx
@@ -3,18 +3,21 @@ import HeaderAdm from "../components/Header/HeaderAdm";
 import Title from "../components/NonInteractable/Title";
 import NavBarEstatisticaEAnalise from "../components/Header/NavBarEstatisticaEAnalise"
 import Subtitle from "../components/NonInteractable/Subtitle";
+<canvas id="grafico"></canvas>
 
 export default function RelatorioEAnalise() {
-        
+
     return (
         <>
             <HeaderAdm width={25.9} height={25.4} color="" />
-            <Title tag="h1" text="Estatísticas e análises"/>
-            <NavBarEstatisticaEAnalise/>
-            <div style={{display: "flex", flexDirection: "row", alignItems: "center", gap: "490px"}}>
-            <Subtitle text="dados totais" lineDirection="left"/> {/**ainda não consegui alinhar a barra a esquerda */}
-            <p className="exportdatas">EXPORTAR DADOS</p>
+            <Title tag="h1" text="Estatísticas e análises" />
+            <NavBarEstatisticaEAnalise />
+            <div style={{ display: "flex", flexDirection: "row", alignItems: "center", gap: "490px" }}>
+                <Subtitle text="dados totais" lineDirection="left" /> {/**ainda não consegui alinhar a barra a esquerda */}
+                <p className="exportdatas">EXPORTAR DADOS</p>
             </div>
+            
+
         </>
     );
 }
