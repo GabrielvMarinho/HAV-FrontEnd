c77ad4c8d89a1bb718f1df2d8f7f9929eda33651
RafaelHR12 <147559750+RafaelHR12@users.noreply.github.com>
2025-03-14 14:15:34 -0300

    Commit para dar push da dev

--- app/Validators/PropertyValidator.tsx
+++ app/Validators/PropertyValidator.tsx
@@ -1,8 +1,11 @@
 import { z } from "zod";
 
 export const newProperty = z.object({
-   cep: z.string().min(1, { message: "* Campo obrigatório" })
-        .regex(/^\d{8}$/, { message: "* 8 dígitos numéricos" }), // Apenas números
+        cep: z.string().min(1, { message: "* Campo obrigatório" })
+                .regex(/^\d{8}$/, { message: "* 8 dígitos numéricos" }), // Apenas números
+        title: z.string().min(1, {message: "* Campo obrigatório"}),
+        propertyDescription: z.string().min(1, {message: "* Campo obrigatório"}),
+        
 });
 
 export type newProperty = z.infer<typeof newProperty>;
