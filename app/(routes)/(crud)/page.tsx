2160e767dffcb0129e296882bdde5a25f8298678
Eduardo Vizoni do Prado <145796079+EduardoVizoni@users.noreply.github.com>
2025-03-17 16:06:23 -0300

    Test: Implementando cards de imóvel
    Implementando cards de imóvel para análise

--- app/(routes)/(crud)/page.tsx
+++ app/(routes)/(crud)/page.tsx
@@ -14,6 +14,7 @@ import HeaderOptions from "../../components/Header/HeaderOptions";
 import Modal from "@/app/components/Modal/Modal";
 import FormTestInput from "@/app/components/Forms/FormTestInput";
 import PropertyPrice from "../../components/NonInteractable/PropertyPrice";
+import CardImovel from "@/app/components/Cards/CardImovel"
 
 export default function Home() {
   
@@ -26,6 +27,8 @@ export default function Home() {
       <Title tag="h1" text="titulo"/>
       <FormTestInput/>
       <PropertyPrice PromotionalPrice="R$2.000.000,00" ActualPrice="R$1.999.999,99" Taxes="R$900,00" Purpose="vendaPromocao"/>
+      <CardImovel bairro="Bairro czerniewicz" cidade="Jaraguá do Sul" valor="R$200.000,00" infoParcela="" quantQuartos={2} quantBanheiros={2} quantSala={1} informationStatus="Lançamento" category="Venda"/>
+      <CardImovel bairro="Amizade" cidade="Jaraguá do Sul" valor="R$1.000,00" infoParcela="/mês" quantQuartos={2} quantBanheiros={2} quantSala={2} informationStatus="Lançamento" category="Locação"/>
       <Footer/>
 
 
