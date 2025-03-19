d67c11f13d8aedb96f0ca7864879a4665bf39d25
Eduardo Vizoni do Prado <vizoni.prado@gmail.com>
2025-03-19 14:52:06 -0300

    Refactor: Refatorando a lógica de 'PropertyPrice'
    Mudando a lógica e ajustando o método de implementação

--- app/models/Property/PropertySpecific.ts
+++ app/models/Property/PropertySpecific.ts
@@ -1,6 +1,10 @@
 interface PropertySpecific{
     id: string;
     address: string;
+    PromotionalPrice: number;
+    ActualPrice: number;
+    Taxes: number;
+    Purpose: string; // venda, vendaPromocao, locacao
     bedroom: number;
     bathroom: number;
     garage :number;
