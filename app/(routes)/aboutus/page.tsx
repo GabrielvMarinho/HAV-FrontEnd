9f4124d43475703778be741cd3061ee27e286fed
RafaelHR12 <147559750+RafaelHR12@users.noreply.github.com>
2025-03-18 21:40:26 -0300

    Página About us
    Continuação da construção da página About Us.

--- app/(routes)/aboutus/page.tsx
+++ app/(routes)/aboutus/page.tsx
@@ -1,5 +1,8 @@
 import Footer from "@/app/components/Footer/Footer";
 import HeaderAdm from "@/app/components/Header/HeaderAdm";
+import HavLogoAboutUs1 from "@/app/components/IconsTSX/havLogoAboutUs1";
+import HavTextAboutUs from "@/app/components/IconsTSX/HAVTextAboutUs";
+import "../aboutus/css/style.css"
 import Title from "@/app/components/NonInteractable/Title";
 
 export default function page() {
@@ -8,7 +11,7 @@ export default function page() {
         <>
             <HeaderAdm />
             <Title tag={"h1"} text={"sobre a hav"} />
-            <div>
+            <div style={{ display: "flex", flexDirection: "column", gap: "20px", width: "60%" }}>
                 <p>
                     A Hav é uma imobiliária dedicada a transformar a experiência de compra, venda e locação de
                     imóveis em algo simples, confiável e transparente.
@@ -19,13 +22,105 @@ export default function page() {
                 </p>
                 <p>
                     Por isso, trabalhamos lado a lado com nossos clientes, buscando as melhores oportunidades e
-                    proporcionando uma experiência diferenciada, com atendimento profissional, ética e 
-                    compromisso.Nossa missão é simplificar o processo imobiliário, oferecendo 
-                    suporte em todas as etapas. Com amplo conhecimento do mercado e um portfólio selecionado, 
-                    na Hav você encontra imóveis que se destacam pela qualidade, ocalização e potencial de 
+                    proporcionando uma experiência diferenciada, com atendimento profissional, ética e
+                    compromisso.Nossa missão é simplificar o processo imobiliário, oferecendo
+                    suporte em todas as etapas. Com amplo conhecimento do mercado e um portfólio selecionado,
+                    na Hav você encontra imóveis que se destacam pela qualidade, ocalização e potencial de
                     valorização.
                 </p>
             </div>
+            <div style={{ width: "60%", marginTop: "20px" }}>
+                <p style={{ color: "var(--box-red-pink)", fontWeight: "bolder" }}>
+                    HAV, O SEU SONHO COMEÇA AQUI!
+                </p>
+            </div>
+            <div className="logoAboutus" style={{
+                backgroundColor: "var(--text-light-red)",
+                borderRadius: "100%", padding: "15px", marginLeft: "500px"
+            }}>
+                <HavLogoAboutUs1 width={75} height={75} color={"var(--icon-footer-color)"}></HavLogoAboutUs1>
+            </div>
+            <div className="textAboutus" style={{
+                backgroundColor: "var(--text-light-red)",
+                borderRadius: "100%", padding: "10px", marginLeft: "200px"
+            }}>
+                <HavTextAboutUs width={60} height={60} color={"var(--icon-footer-color)"}></HavTextAboutUs>
+            </div>
+
+            <div style={{
+                display: "flex", flexDirection: "column", gap: "20px", width: "100%",
+                marginTop: "120px"
+            }}>
+
+                <div style={{ display: "flex", flexDirection: "row", gap: "50px" }}>
+
+                    <div style={{
+                        display: "flex", flexDirection: "column", gap: "20px",
+                        marginLeft: "166px"
+                    }}>
+                        <div className="titleContainer">
+                            <h3 className="titleAboutus">MISSÃO</h3>
+                            <div className="titleLineAboutus"></div>
+                        </div>
+                        <div style={{ width: "70%" }}>
+                            <p>
+                                Nossa missão é simplificar o processo imobiliário,
+                                oferecendo suporte em todas as etapas.
+                            </p>
+                        </div>
+                    </div>
+                    <div style={{
+                        display: "flex", flexDirection: "column", gap: "20px",
+                        backgroundColor: "var(--back-ground-light-red)", borderRadius: "5px",
+                        padding: "10px 166px 10px 40px"
+                    }}>
+                        <div className="titleContainer">
+                            <h3 className="titleAboutus">VALORES</h3>
+                            <div className="titleLineAboutus"></div>
+                        </div>
+                        <div style={{ width: "70%" }}>
+                            <p>
+                                Nossa missão é simplificar o processo imobiliário,
+                                oferecendo suporte em todas as etapas.
+                            </p>
+                        </div>
+                    </div>
+                </div>
+
+                <div style={{ display: "flex", flexDirection: "row", gap: "75px" }}>
+
+                    <div style={{
+                        display: "flex", flexDirection: "column", gap: "20px",
+                        backgroundColor: "var(--back-ground-light-red)", borderRadius: "5px",
+                        padding: "10px 0px 10px 166px"
+                    }}>
+                        <div className="titleContainer">
+                            <h3 className="titleAboutus">VISÃO</h3>
+                            <div className="titleLineAboutus"></div>
+                        </div>
+                        <div style={{ width: "70%" }}>
+                            <p>
+                                Nossa missão é simplificar o processo imobiliário,
+                                oferecendo suporte em todas as etapas.
+                            </p>
+                        </div>
+                    </div>
+                    <div style={{
+                        display: "flex", flexDirection: "column", gap: "20px", marginRight: "166px"
+                    }}>
+                        <div className="titleContainer">
+                            <h3 className="titleAboutus">CONFIANÇA</h3>
+                            <div className="titleLineAboutus"></div>
+                        </div>
+                        <div style={{ width: "70%" }}>
+                            <p>
+                                Nossa missão é simplificar o processo imobiliário,
+                                oferecendo suporte em todas as etapas.
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div >
             <Footer />
         </>
     )
--- app/components/NonInteractable/Title.tsx
+++ app/components/NonInteractable/Title.tsx
@@ -1,17 +1,17 @@
 import './css/style.css';
 
-export default function Title(props: {tag :string, text :string}){
+export default function Title(props: { tag: string, text: string }) {
     //retorna um h1 caso ele seja o primeiro título da página
-    if(props.tag=="h1"){
+    if (props.tag == "h1") {
         return (
-            <div className="title" style={{display: "flex", flexDirection: "column"}}>
+            <div className="title" style={{ display: "flex", flexDirection: "column" }}>
                 <h1 className='sizeTitle'>{props.text.toUpperCase()}</h1>
                 <div className="titleLine"></div>
             </div>
         );
     }
-    return ( 
-        <div className="title" style={{display: "flex", flexDirection: "column"}}>
+    return (
+        <div className="title" style={{ display: "flex", flexDirection: "column" }}>
             <h2 className='sizeTitle'>{props.text.toUpperCase()}</h2>
             <div className="titleLine"></div>
         </div>
