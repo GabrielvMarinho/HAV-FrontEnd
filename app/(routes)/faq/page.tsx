1e338658fc413bb32e7f4cdb92373939bf5fde64
Eduardo Vizoni do Prado <vizoni.prado@gmail.com>
2025-03-19 14:32:50 -0300

    Feat: Comelando a página FAQ

---
+++ app/(routes)/faq/page.tsx
@@ -0,0 +1,10 @@
+import Title from "@/app/components/NonInteractable/Title";
+
+export default function page(){
+    return(
+        <>
+            <Title tag={"h1"} text={"perguntas frequentes"}/>
+            <p>Aqui você encontra respostas rápidas e práticas para as dúvidas mais comuns sobre nossos produtos e serviços.</p>
+        </>
+    )
+}
