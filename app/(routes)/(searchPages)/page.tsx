a8daebffc3b431c0ae0c6061f966f26288a7d190
biancavaz <147559714+biancavaz@users.noreply.github.com>
2025-03-19 19:46:40 -0300

    arrumado mobile

--- app/(routes)/(searchPages)/page.tsx
+++ app/(routes)/(searchPages)/page.tsx
@@ -32,7 +32,7 @@ export default function Home() {
 
   // const session = getServerSession()
   
-  console.log(session)
+  // console.log(session)
   const images = [
     "https://images.unsplash.com/photo-1726607962647-84ec2451d553?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxmZWF0dXJlZC1waG90b3MtZmVlZHwxfHx8ZW58MHx8fHx8",
     "https://plus.unsplash.com/premium_photo-1734543942868-2470c4cba7b5?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHwyfHx8ZW58MHx8fHx8",
