180f93ec64b2c4b7d4109fc27b1ed8a06f0d667d
biancavaz <147559714+biancavaz@users.noreply.github.com>
2025-03-25 16:58:05 -0300

    terminando o design da página de login

--- app/(routes)/login/page.tsx
+++ app/(routes)/login/page.tsx
@@ -2,6 +2,9 @@ import HavLogo from "@/app/components/IconsTSX/HavLogoLight";
 import "../login/style/style.css";
 import Logo from "@/app/components/IconsTSX/logo";
 import LogoFundo from "@/app/components/IconsTSX/logoFundo";
+import Title from "@/app/components/NonInteractable/Title";
+import InputText from "@/app/components/Inputs/InputText";
+import Button from "@/app/components/Inputs/Button";
 
 
 export default function login(){
@@ -10,15 +13,29 @@ export default function login(){
         <div className= "container">
             <div className= "diagonal"> </div>
 
+            <div className="form-box">
+                <div className="tituloLogin">
+                <Title tag="h1" text="LOGIN"/>
+                </div>
+                <form>
+                    <InputText name="name" size="medium" id="user" text="E-mail" placeholder="Digite seu e-mail"/>
+                    <InputText name="name" size="medium" id="pass" text="Senha" placeholder="Digite sua senha"/>
+                    <p className="EsqueceuSenha">Esqueceu a senha?</p>
+                    <div className="botao">
+                        <Button name="button" size="small" text="Entrar" /> 
+                    </div>
+                    <p className="ou">ou</p>
+                </form>
+             </div>
+
             <div className="logoFundo">
-                <LogoFundo width="800" height="850"/>
+                <LogoFundo width="100%" height="850"/>
             </div>
 
             <div className="logo"> 
-                <Logo/>
+                <Logo width="70%" height="450"/>
             </div>
         </div>
-
 
         </>
     )
