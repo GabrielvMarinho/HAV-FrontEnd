c6283b58d16be0a3d2836ff66658c606b8e120c3
biancavaz <147559714+biancavaz@users.noreply.github.com>
2025-03-24 21:53:07 -0300

    páginas feitas a base e começado a fazer a página de login

---
+++ app/(routes)/configuration/page.tsx
@@ -0,0 +1,12 @@
+import HeaderAdm from "@/app/components/Header/HeaderAdm";
+import Title from "@/app/components/NonInteractable/Title";
+
+
+export default function configuration(){
+    return(
+        <>
+        <HeaderAdm/>
+        <Title tag="h1" text="Configurações"/>
+        </>
+    )
+}
---
+++ app/(routes)/profile/page.tsx
@@ -0,0 +1,11 @@
+import HeaderAdm from "@/app/components/Header/HeaderAdm";
+import Title from "@/app/components/NonInteractable/Title";
+
+export default function profile (){
+    return(
+        <>
+        <HeaderAdm/>
+        <Title tag="h1" text="perfil"/>
+        </>
+    )
+}
