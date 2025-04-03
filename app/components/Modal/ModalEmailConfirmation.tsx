aea0a563d98d7e579195abcfa86f4d23829ad102
biancavaz <147559714+biancavaz@users.noreply.github.com>
2025-04-03 12:15:56 -0300

    modal do email esta pronto, sem funcionalidade ainda

--- app/components/Modal/ModalEmailConfirmation.tsx
+++ app/components/Modal/ModalEmailConfirmation.tsx
@@ -6,6 +6,11 @@ import Clock from '../IconsTSX/Clock';
 import CategoryCardImovel from '../Information/CategoryCardImovel';
 import Button from '../Inputs/Button';
 import './css/style.css';
+import InputText from '../Inputs/InputText';
+import { textFields } from '../globalFormsConfig/InputTextConfig';
+import { NewEditorOrAdm } from '@/app/Validators/EditorOrAdmValidator';
+import InputTextLogin from '../Inputs/InputTextLogin';
+import Envelope from '../IconsTSX/Envelope';
 
 
 export default function ModalEmailConfirmation () {
@@ -15,29 +20,45 @@ export default function ModalEmailConfirmation () {
         
         if(page==0){
             return(
-                <div>
+                <div className='containerModalEmailConfirmation'>
                     
-                        <h2> Alteração do E-mail</h2>
-                        <div> linha </div>
-                        <div> textoooooooooooooooooo </div>
+                    <div className='subContainerModalEmailConfirmation'>
+                        <h2 className='titleModalEmailConfirmation'> Alteração do E-mail</h2>
+                        <div className='lineModalEmailConfirmation'>  </div>
+                        <div className='textModalEmailConfirmation'> Um código de confirmação será enviado para este E-mail para validá-lo </div>
+                    </div>
+                    <div className='inputModalEmailConfirmation'>  
+                        <InputText name="name" size="large" id="user" text="E-mail" placeholder="Digite seu e-mail"/>
+                    </div>
 
-                        <div className="modalNewSchedule">
-                    
-                    <Calendar width={80} height={80} color="var(--box-white)"></Calendar>
-                    <Button size={"small"} hover={"lightHover"} background={"var(--button-color-reverse)"} color={"var(--text-red-pink)"} text={"Agende sua visita"} onClick={() =>{setPage(1)}}></Button>
-    
-                </div>
+                    <div className="buttonModalEmailConfirmation">
+                        <Button size={"small"} hover={"lightHover"} background={"var(--button-color-reverse)"} color={"var(--text-red-pink)"} text={"Enviar Código"} onClick={() =>{setPage(1)}}></Button>
+                    </div>
 
-                   
                 </div>
             )
         }
         if(page==1){
             
             return(
-               <div>
-                oiiiieeeeeeeeeeee
-               </div>
+                <div className='containerModalEmailConfirmation'>
+                    
+                <div className='subContainerModalEmailConfirmation'>
+                    <h2 className='titleModalEmailConfirmationSecond'> Insira o código de 6 dígitos</h2>
+                    <div className='lineModalEmailConfirmation'>  </div>
+                    <div className='textModalEmailConfirmationSecond'> Verifique se há um código de verificação para ex*****@gmail.com.  <a className='mudarModalEmail'> Mudar </a> </div>
+                </div>
+                <div className='inputModalEmailConfirmation'>  
+                    <InputText name="name" size="large" id="user" text="Código de 6 dígitos" placeholder="Digite seu e-mail"/>
+                </div>
+                <h2 className='codigoModalEmailConfirmation'> Reenviar código </h2>
+
+                <div className="buttonModalEmailConfirmationSecond">
+                    <Button size={"small"} hover={"lightHover"} background={"var(--button-color-reverse)"} color={"var(--text-red-pink)"} text={"Confirmar"} onClick={() =>{setPage(1)}}></Button>
+                </div>
+                <div className='textInformationModalEmailConfirmation'> Se não encontrar o código na sua caixa de entrada, verifique a pasta de spam. Caso não esteja lá, o e-mail pode não ter sido confirmado ou pode não estar associado a uma conta HAV. </div>
+
+            </div>
             )
         }
         
