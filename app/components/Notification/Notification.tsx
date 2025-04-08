0d86da32285c4b970d6c697d4445e5c762befee5
Nathan João Lima <147559563+nathanjoao@users.noreply.github.com>
2025-04-08 20:56:32 -0300

    estilizando a notificação

--- app/components/Notification/Notification.tsx
+++ app/components/Notification/Notification.tsx
@@ -4,6 +4,8 @@ import { useEffect, useState } from 'react';
 import { useParams } from 'next/navigation';
 import SockJS from 'sockjs-client';
 import { Client } from '@stomp/stompjs';
+import HorizontalLine from "@/app/components/NonInteractable/HorizontalLine"
+import "./css/style.css"
 
 type MessageDTO = {
     recipient: number
@@ -12,54 +14,64 @@ type MessageDTO = {
 }
 
 const Notification = () => {
-  const params = useParams();
-  const idUser = params.id;
-  const [messages, setMessages] = useState<MessageDTO[]>([]) // para poder aparecer todas as notificações
+    const params = useParams();
+    const idUser = params.id;
+    const [messages, setMessages] = useState<MessageDTO[]>([]) // para poder aparecer todas as notificações
 
-  useEffect(() => {
-    if (!idUser) return;
+    useEffect(() => {
+        if (!idUser) return;
 
-    const stompClient = new Client({
-      webSocketFactory: () => new SockJS('http://localhost:9090/ws'),
-      onConnect: () => {
-        console.log('Conectado ao WebSocket');
-        stompClient.subscribe(`/topic/mensagens${idUser}`, (mensagem) => {
-          const body: MessageDTO = JSON.parse(mensagem.body);
-          console.log('Mensagem recebida:', body);
-          setMessages(prev => [...prev, body]);
+        const stompClient = new Client({
+            webSocketFactory: () => new SockJS('http://localhost:9090/ws'),
+            onConnect: () => {
+                console.log('Conectado ao WebSocket');
+                stompClient.subscribe(`/topic/mensagens${idUser}`, (mensagem) => {
+                    const body: MessageDTO = JSON.parse(mensagem.body);
+                    console.log('Mensagem recebida:', body);
+                    setMessages(prev => [...prev, body]);
+                });
+            },
+            onStompError: (frame) => {
+                console.error('Erro no STOMP:', frame);
+            }
         });
-      },
-      onStompError: (frame) => {
-        console.error('Erro no STOMP:', frame);
-      }
-    });
 
-    stompClient.activate();
+        stompClient.activate();
 
-    return () => {
-      stompClient.deactivate();
-    };
-  }, [idUser]);
+        return () => {
+            stompClient.deactivate();
+        };
+    }, [idUser]);
 
-  return (
-    <div>
-        {messages.length === 0 ? (
-            <p>Por enquanto não há mensagens</p>
-        ) : (
-            <ul>
-                {messages.map((message, index) => (
-                    <li key={index}>
-                        <p>De:</p>{message.senderName}
-                        <br />
-                        <p>Menssagem:</p>{message.content}
-                        <br />
-                        
-                    </li>
-                ))}
-            </ul>
-        )}
-    </div>
-  );
+    return (
+        <div>
+            {messages.length === 0 ? (
+                <p>Por enquanto não há mensagens</p>
+            ) : (
+                <ul className='boxNotification'>
+
+                    {messages.map((message, index) => (
+                        <li key={index} style={{ display: 'flex', flexDirection: 'column' }}>
+                            <HorizontalLine size={930} color='#0F0F0F ' />
+                            <section className='containerNotification'>
+                                {/*Div para ajustar as informações da mensagem -> */}
+                                <div className='groupInfos'>
+                                    <div className='bola'></div>
+                                    <div style={{ display: 'flex', flexDirection: "column", gap: "5px" }}>
+                                        <p className='identificator'>{message.senderName}</p>
+                                        <p>{message.content}</p>
+                                    </div>
+                                </div>
+                                <label className='newMessageLabel'>nova mensagem</label>
+                            </section>
+                            <HorizontalLine size={930} color='#0F0F0F ' />
+                        </li>
+                    ))}
+
+                </ul>
+            )}
+        </div>
+    );
 };
 
 export default Notification;
