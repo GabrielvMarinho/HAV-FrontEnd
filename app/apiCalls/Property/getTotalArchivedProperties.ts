85f9539b0ce7c50530dc215ba6547c13e4d09eb5
Nathan João Lima <147559563+nathanjoao@users.noreply.github.com>
2025-04-24 18:46:33 -0300

    funcionando primeiro gráfico de imoveis

--- app/apiCalls/Property/getTotalArchivedProperties.ts
+++ app/apiCalls/Property/getTotalArchivedProperties.ts
@@ -1,8 +1,9 @@
-const BASE_URL = "http://localhost:9090/property"
-
 export default async function(){
+    const BASE_URL = "http://localhost:9090/property/getQuantityOfArchivedProperties"
     try{
-        const response = await fetch(`${BASE_URL}/getQuantityOfArchivedProperties`)
+        const response = await fetch(BASE_URL, {
+            credentials: "include"
+        })
         if(!response.ok) throw new Error("Erro ao buscar o total de propriedades arquivadas")
         return await response.json();
     }catch(e){
