cca52ea733b3428982f274b088cb01b8d4af3963
biancavaz <147559714+biancavaz@users.noreply.github.com>
2025-04-25 11:49:57 -0300

    arrumei a busca de imóveis da dev

--- app/components/Information/TableList.tsx
+++ app/components/Information/TableList.tsx
@@ -16,12 +16,11 @@ import ArrowBack from '../IconsTSX/ArrowBack';
 import PageManager from '../Inputs/PageManager';
 import React from 'react';
 import getByParamsRealtors from '@/app/apiCalls/Realtor/getByParamsRealtors';
-import { Customer } from '@/app/models/Customer/Customer';
 
 
-export default function TableList(props: { totalPages: number; changeArchivedStatus: (ids: string[]) => Promise<void>; deleteFunction: (ids: string[]) => Promise<void>; archived: boolean; context: string; size: string, titles: string[], data: any[] }) {
+export default function TableList(props: {totalPages :number; changeArchivedStatus :(ids: string[]) => Promise<void>; deleteFunction: (ids: string[]) => Promise<void>; archived :boolean; context :string; size :string, titles :string[], data :any[]}){
 
-
+    
     // useEffect(() => {
     //     const fetch  = async () => {
     //         const response = await getByParamsRealtors(undefined, undefined, undefined, undefined, undefined, false, 0);
@@ -29,11 +28,11 @@ export default function TableList(props: { totalPages: number; changeArchivedSta
     //     }
     //     fetch();
     // }, []);
-
+    
     const confirmDelete = async () => {
         const selectedIds = JSON.parse(localStorage.getItem('selectedManage') || "[]");
         if (selectedIds.length > 0) {
-            await props.deleteFunction(selectedIds);
+            await props.deleteFunction(selectedIds); 
             localStorage.removeItem("selectedManage")
             setIsDeleteModalOpen(false);
 
@@ -48,45 +47,45 @@ export default function TableList(props: { totalPages: number; changeArchivedSta
     const router = useRouter();
 
 
-    const archiveRoute = function () {
+    const archiveRoute = function (){
         router.push(`${window.location.pathname}/archived`);
     }
-    const notArchiveRoute = function () {
+    const notArchiveRoute = function(){
         router.push(window.location.pathname.replace(/\/archived$/, ""));
     }
     //EDIT RELATED
-    const editFunction = function () {
+    const editFunction = function(){
         const selectedId = JSON.parse(localStorage.getItem('selectedManage') || "[]");
         localStorage.removeItem("selectedManage")
         router.push(`${window.location.pathname}/edit/${selectedId[0]}`);
     }
 
     //ADD RELATED
-    const addFunction = function () {
-
+    const addFunction = function(){
+        
         router.push(`${window.location.pathname}/add`);
     }
 
     //DELETE RELATED
     const [isDeleteModalOpen, setIsDeleteModalOpen] = useState(false);
-    const openDeleteModal = function () {
+    const openDeleteModal = function(){
         setIsDeleteModalOpen(true)
     }
-
+    
     //ARCHIVE RELATED
     const [isArchiveModalOpen, setIsArchiveModalOpen] = useState(false)
 
 
-    const openArchiveModal = function () {
+    const openArchiveModal = function(){
         setIsArchiveModalOpen(true)
     }
 
-    const changeArchivedStatus = async function () {
+    const changeArchivedStatus = async function (){
 
         const selectedIds = JSON.parse(localStorage.getItem('selectedManage') || "[]");
-
+        
         if (selectedIds.length > 0) {
-            await props.changeArchivedStatus(selectedIds);
+            await props.changeArchivedStatus(selectedIds); 
             localStorage.removeItem("selectedManage")
             setIsArchiveModalOpen(false);
 
@@ -96,7 +95,7 @@ export default function TableList(props: { totalPages: number; changeArchivedSta
     }
 
     //GRAPHS RELATED
-    const graphFunction = function () {
+    const graphFunction = function(){
         router.push(`/reports/userReports`);
     }
 
@@ -109,11 +108,11 @@ export default function TableList(props: { totalPages: number; changeArchivedSta
         localStorage.removeItem('selectedManage');
     }
 
-
+    
     localStorage.setItem('previousPathname', pathname);
-    useEffect(() => {
-        localStorage.removeItem('selectedManage')
-    }, [pathname])
+        useEffect(() =>{
+            localStorage.removeItem('selectedManage')
+        }, [pathname])
 
     const [selected, setSelected] = useState<string[]>(() => {
         const saved = localStorage.getItem('selectedManage');
@@ -135,135 +134,114 @@ export default function TableList(props: { totalPages: number; changeArchivedSta
 
     //PAGEABLE RELATED
 
-    const titleToKeyMap: Record<string, keyof Customer> = {
-        "cpf": "cpf",
-        "nome": "name",
-        "email": "email",
-        "telefone": "cellphone",
-        "status": "statusText"
-    };
-
 
     return (
         <>
-
-            <div>
-                <table className="tableListData">
-                    <thead>
-                        <tr>
+        
+        <div>
+        <table className="tableListData">
+            <thead>
+                    <tr>
                             <div className='tableListLineHide marginSelectBox'></div>
 
-                            {props.titles.map((text, index) =>
-                                <th key={index}>{text.toLocaleUpperCase()}</th>
-                            )}
+                            {props.titles.map( (text, index) => 
+                                    <th key={index}>{text.toLocaleUpperCase()}</th>
+                                )}
 
-                        </tr>
-                    </thead>
+                    </tr>
+            </thead>
+            
+            <tbody>
+                {props.data && props.data.map((obj, index) =>
 
-                    <tbody>
-                        {props.data && props.data.map((obj, index) =>
+                    <React.Fragment key={Object.values(obj)[0] || index}>
+                        <div className='tableListLine'></div>
 
-                            <React.Fragment key={Object.values(obj)[0] || index}>
-                                <div className='tableListLine'></div>
+                        <tr className={selected.includes(Object.values(obj)[0])?"selectedRow tableRows":"tableRows"}>
+                            <td style={{all: "unset", margin:"3px"}}>
+                                <div className='marginSelectBox'>
+                                
 
-                                <tr className={selected.includes(Object.values(obj)[0]) ? "selectedRow tableRows" : "tableRows"}>
-                                    <td style={{ all: "unset", margin: "3px" }}>
-                                        <div className='marginSelectBox'>
-
-
-                                            <input
-                                                className='checkbox'
-                                                type="checkbox"
-                                                checked={selected.includes(Object.values(obj)[0])}
-                                                onChange={() => handleSelect(Object.values(obj)[0])}
-                                            />
+                                <input
+                                className='checkbox'
+                                    type="checkbox"
+                                    checked={selected.includes(Object.values(obj)[0])}
+                                    onChange={() => handleSelect(Object.values(obj)[0])}
+                                />
 
 
+                                </div>
+                            </td>
+                            {Object.entries(obj).slice(1).map(([key, value]) => (
+                                    <td key = {key}>
+                                        <div>
+                                            {key === "price" ? `R$${value.toLocaleString('en-US').replace(/,/g, '.')}` : value==null?"----":value}
                                         </div>
                                     </td>
-                                    {/* {Object.entries(obj).slice(1).map(([key, value]) => (
-                                        <td key={key}>
-                                            <div>
-                                                {key === "price" ? `R$${value.toLocaleString('en-US').replace(/,/g, '.')}` : value == null ? "----" : value}
-                                            </div>
-                                        </td>
 
-                                    ))} */}
-                                    {props.titles.map((title) => {
-                                        const key = titleToKeyMap[title]; // pega o campo real do objeto
-                                        const value = obj[key];
+                                ))}
+                        </tr>
+                        </React.Fragment>
 
-                                        return (
-                                            <td key={title}>
-                                                <div>
-                                                    {value == null ? "----" : value}
-                                                </div>
-                                            </td>
-                                        );
-                                    })}
+                    )}
+            
+            </tbody>
+            
+        </table>
+        <PageManager totalPages={props.totalPages}></PageManager>
+        </div>
+        <div className="actionButtons"> 
+            {props.archived===false? (
+                <>
+                {props.context === 'admin'?(
+                <ActionButton onClick={graphFunction} className={`${selected.length==0?"darkHover actionButtonHover":"nonClickableButton"} changeRouteButton `} Icon={Graphic}  />
 
-                                </tr>
-                            </React.Fragment>
+                ):("")}
+                <ActionButton onClick={archiveRoute}  className={`${selected.length==0?"darkHover actionButtonHover":"nonClickableButton"} changeRouteButton `} Icon={Folder}  />
+                <ActionButton onClick={addFunction} className={`${selected.length==0?"darkHover actionButtonHover":"nonClickableButton"} changeRouteButton `} Icon={MoreSignal} />
+                <ActionButton onClick={selected.length==1?editFunction:""} className={`${selected.length==1?"darkHover actionButtonHover":"nonClickableButton"} actionSelectedButton `} Icon={Pencil}  />
+                {/* <ActionButton onClick={selected.length>0?openDeleteModal:""} className={`${selected.length>0?"darkHover actionButtonHover":"nonClickableButton"} actionSelectedButton `} Icon={Trashcan} /> */}
+                <ActionButton onClick={selected.length>0?openArchiveModal:""} className={`${selected.length>0?"darkHover actionButtonHover":"nonClickableButton"} actionSelectedButton `} Icon={ArchiveIn}  />
+                <Modal content={
+                    <div className="containerModal">
+                        <h1 className="titleModal">DESEJA REMOVER? </h1>
+                        <p className="descModal"> Ao confirmar, os dados selecionados serão excluídos.</p>
+                    </div>
+                } id="deleteModal" isOpen={isDeleteModalOpen} onClose={() => setIsDeleteModalOpen(false)} onConfirm={confirmDelete} />
+                
+                <Modal content={
+                    <div className="containerModal">
+                        <h1 className="titleModal">DESEJA ARQUIVAR? </h1>
+                        <p className="descModal"> Ao confirmar, os dados desarquivados serão arquivados.</p>
+                    </div>
+                } id="archiveModal" isOpen={isArchiveModalOpen} onClose={() => setIsArchiveModalOpen(false)} onConfirm={changeArchivedStatus} />
 
-                        )}
-
-                    </tbody>
-
-                </table>
-                <PageManager totalPages={props.totalPages}></PageManager>
-            </div>
-            <div className="actionButtons">
-                {props.archived === false ? (
-                    <>
-                        {props.context === 'admin' ? (
-                            <ActionButton onClick={graphFunction} className={`${selected.length == 0 ? "darkHover actionButtonHover" : "nonClickableButton"} changeRouteButton `} Icon={Graphic} />
-
-                        ) : ("")}
-                        <ActionButton onClick={archiveRoute} className={`${selected.length == 0 ? "darkHover actionButtonHover" : "nonClickableButton"} changeRouteButton `} Icon={Folder} />
-                        <ActionButton onClick={addFunction} className={`${selected.length == 0 ? "darkHover actionButtonHover" : "nonClickableButton"} changeRouteButton `} Icon={MoreSignal} />
-                        <ActionButton onClick={selected.length == 1 ? editFunction : ""} className={`${selected.length == 1 ? "darkHover actionButtonHover" : "nonClickableButton"} actionSelectedButton `} Icon={Pencil} />
-                        {/* <ActionButton onClick={selected.length>0?openDeleteModal:""} className={`${selected.length>0?"darkHover actionButtonHover":"nonClickableButton"} actionSelectedButton `} Icon={Trashcan} /> */}
-                        <ActionButton onClick={selected.length > 0 ? openArchiveModal : ""} className={`${selected.length > 0 ? "darkHover actionButtonHover" : "nonClickableButton"} actionSelectedButton `} Icon={ArchiveIn} />
-                        <Modal content={
-                            <div className="containerModal">
-                                <h1 className="titleModal">DESEJA REMOVER? </h1>
-                                <p className="descModal"> Ao confirmar, os dados selecionados serão excluídos.</p>
-                            </div>
-                        } id="deleteModal" isOpen={isDeleteModalOpen} onClose={() => setIsDeleteModalOpen(false)} onConfirm={confirmDelete} />
-
-                        <Modal content={
-                            <div className="containerModal">
-                                <h1 className="titleModal">DESEJA ARQUIVAR? </h1>
-                                <p className="descModal"> Ao confirmar, os dados desarquivados serão arquivados.</p>
-                            </div>
-                        } id="archiveModal" isOpen={isArchiveModalOpen} onClose={() => setIsArchiveModalOpen(false)} onConfirm={changeArchivedStatus} />
-
-                    </>
-                ) : (
-                    <>
-                        <ActionButton onClick={notArchiveRoute} className={`${selected.length == 0 ? "darkHover actionButtonHover" : "nonClickableButton"} changeRouteButton `} Icon={ArrowBack} />
-                        <ActionButton onClick={selected.length == 1 ? editFunction : ""} className={`${selected.length == 1 ? "darkHover actionButtonHover" : "nonClickableButton"} actionSelectedButton `} Icon={Pencil} />
-                        {/* <ActionButton onClick={selected.length>0?openDeleteModal:""} className={`${selected.length>0?"darkHover actionButtonHover":"nonClickableButton"} actionSelectedButton `} Icon={Trashcan} /> */}
-                        <ActionButton onClick={selected.length > 0 ? openArchiveModal : ""} className={`${selected.length > 0 ? "darkHover actionButtonHover" : "nonClickableButton"} actionSelectedButton `} Icon={ArchiveOut} />
-
-                        <Modal content={
-                            <div className="containerModal">
-                                <h1 className="titleModal">DESEJA DESARQUIVAR? </h1>
-                                <p className="descModal"> Ao confirmar, os dados arquivados serão desarquivados.</p>
-                            </div>
-                        } id="archiveModal" isOpen={isArchiveModalOpen} onClose={() => setIsArchiveModalOpen(false)} onConfirm={changeArchivedStatus} />
-                        <Modal content={
-                            <div className="containerModal">
-                                <h1 className="titleModal">DESEJA REMOVER? </h1>
-                                <p className="descModal"> Ao confirmar, os dados selecionados serão excluídos.</p>
-                            </div>
-                        } id="deleteModal" isOpen={isDeleteModalOpen} onClose={() => setIsDeleteModalOpen(false)} onConfirm={confirmDelete} />
-                    </>
+                </>
+            ) : (
+            <>
+                <ActionButton onClick={notArchiveRoute}  className={`${selected.length==0?"darkHover actionButtonHover":"nonClickableButton"} changeRouteButton `} Icon={ArrowBack}  />
+                <ActionButton onClick={selected.length==1?editFunction:""} className={`${selected.length==1?"darkHover actionButtonHover":"nonClickableButton"} actionSelectedButton `} Icon={Pencil}  />
+                {/* <ActionButton onClick={selected.length>0?openDeleteModal:""} className={`${selected.length>0?"darkHover actionButtonHover":"nonClickableButton"} actionSelectedButton `} Icon={Trashcan} /> */}
+                <ActionButton onClick={selected.length>0?openArchiveModal:""} className={`${selected.length>0?"darkHover actionButtonHover":"nonClickableButton"} actionSelectedButton `} Icon={ArchiveOut}  />
+                
+                <Modal content={
+                    <div className="containerModal">
+                        <h1 className="titleModal">DESEJA DESARQUIVAR? </h1>
+                        <p className="descModal"> Ao confirmar, os dados arquivados serão desarquivados.</p>
+                    </div>
+                } id="archiveModal" isOpen={isArchiveModalOpen} onClose={() => setIsArchiveModalOpen(false)} onConfirm={changeArchivedStatus} />
+                <Modal content={
+                    <div className="containerModal">
+                        <h1 className="titleModal">DESEJA REMOVER? </h1>
+                        <p className="descModal"> Ao confirmar, os dados selecionados serão excluídos.</p>
+                    </div>
+                } id="deleteModal" isOpen={isDeleteModalOpen} onClose={() => setIsDeleteModalOpen(false)} onConfirm={confirmDelete} />
+                </>
                 )}
-
-            </div>
+            
+        </div>
         </>
 
-
+        
     );
 }
